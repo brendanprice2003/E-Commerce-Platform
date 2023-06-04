bc492abd4871257c98ac164e5cc6a8d2ed852c86
Kristaps Eisaks <kristapseisaks03@gmail.com>
2023-06-05 00:22:36 +0100

    Moving scripts
    whoops

--- src/public/js/home.js
+++ src/public/js/home.js
@@ -1,4 +1,30 @@
-
 const log = console.log.bind(console);
 
 log('Hello World');
+
+
+
+//handle the slideshow
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showSlide() {
+    slides.forEach((slide) => {
+        slide.style.display = 'none';
+    });
+
+    slides[currentSlide].style.display = 'flex';
+
+    // Delay the opacity transition for a smooth fade-in effect
+    setTimeout(() => {
+        slides[currentSlide].style.opacity = 1;
+    }, 50);
+}
+
+function nextSlide() {
+    slides[currentSlide].style.opacity = 0;
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide();
+}
+
+setInterval(nextSlide, 5000); // Change slide every 3 seconds
